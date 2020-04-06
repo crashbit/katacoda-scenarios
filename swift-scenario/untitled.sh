437c7ddb416ae8c85b0428d0d3f705932dffddde
German Santos <germans@unam.mx>
2020-04-06 17:33:12 -0500

    Scenario for Swift v 0.0.3

--- swift-scenario/untitled.sh
+++ swift-scenario/untitled.sh
@@ -4,3 +4,5 @@ sudo apt-get -y upgrade
 sudo apt-get -y install
 sudo apt-get -y install libcurl3 libpython2.7 libpython2.7-dev
 sudo apt-get -y install libcurl4-openssl-dev
+wget https://swift.org/builds/swift-5.0.1-release/ubuntu1804/swift-5.0.1-RELEASE/swift-5.0.1-RELEASE-ubuntu18.04.tar.gz
+tar xzf swift-5.0.1-RELEASE-ubuntu18.04.tar.gz
